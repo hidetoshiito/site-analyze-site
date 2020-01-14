02add002278e7813e5997ef596b0dafa433c6aff
hidetoshi.ito <hidetoshi.ito03@gmail.com>
2020-01-14 19:53:59 +0900

    loadingの改善

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,24 +4,10 @@
       text-center
       wrap
     >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          サイト診断
         </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
         <v-text-field
           v-model="target_site"
           :label="fld_site.label"
@@ -30,68 +16,23 @@
           :clearable="fld_site.clearable"
         >
         </v-text-field>
-        <v-btn :loading="loading" :disabled="loading" @click="analyze()" color="success">
+        <v-btn x-large class="mb-3" width="50vw" :loading="loading_all" :disabled="loading_all" @click="analyze()" color="success">
           診断
         </v-btn>
-        <p>{{result_status}}</p>
-        <p>{{result_data}}</p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
+        <div v-if="loading_member.nakcat">
+          <p><v-progress-circular color="primary" indeterminate></v-progress-circular> nakcat's scaning ...</p>
+        </div>
+        <div v-else-if="result_member.nakcat != null">
+          <p>nakcat's result</p>
+          <p>{{result_member.nakcat}}</p>
+        </div>
+        <div v-if="loading_member.mozilla != null">
+          <p><v-progress-circular color="primary" indeterminate></v-progress-circular> mozilla's scaning ...</p>
+        </div>
+        <div v-else-if="result_member.mozilla">
+          <p>mozilla's result</p>
+          <p>{{result_member.mozilla}}</p>
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
@@ -106,9 +47,8 @@ export default {
   name: 'HelloWorld',
 
   data: () => ({
-    loading: false,
-    result_status: '',
-    result_data: '',
+    loading_member: { nakcat: false, mozilla: false },
+    result_member: { nakcat: null, mozilla: null },
     target_site: 'https://int-inc.jp',
     fld_site: { // 診断サイトfieldの設定
       label: '診断サイト',
@@ -116,34 +56,24 @@ export default {
       outlined: true,
       clearable: true,
     },
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-    ],
-    whatsNext: [
-      {},
-    ],
   }),
+  computed: {
+    // nakcat mozilla すべてのスキャンが終わったか？
+    loading_all() {
+      console.log('loading_all start');
+      // true(＝loading中)があるか？
+      const ret = Object.keys(this.loading_member).filter(key => this.loading_member[key] === true);
+      console.log(ret);
+      if (ret.length > 0) {
+        return true;
+      }
+      return false;
+    },
+  },
   mounted() {
     console.log(`${this.$vnode.componentOptions.tag} : mounted start`);
-/*
-    axios.get('https://http-observatory.security.mozilla.org/api/v1/analyze?host=www.mozilla.org')
+    /*
+    axios.post('https://http-observatory.security.mozilla.org/api/v1/analyze?host=www.mozilla.org')
       .then((response) => {
       // 処理成功2xx時のコールバック
         console.log(response.data);
@@ -152,26 +82,25 @@ export default {
         // 処理失敗!not2xx時のコールバック
         console.err(error);
       });
-*/
+    */
   },
   methods: {
     // 診断ボタン押下時の処理
     analyze() {
       console.log('method analyze start');
-      this.loading = true;
+      this.loading_member.nakcat = true;
       axios.get(`https://mysterious-ocean-57311.herokuapp.com/analize.json?host=${this.target_site}&token=password`)
         .then((response) => {
           // 処理成功2xx時のコールバック
           console.dir(response.data);
-          this.result_status = response.data.status;
-          this.result_data = response.data.analize_names;
-          this.loading = false;
+          this.result_member.nakcat = response.data;
+          this.loading_member.nakcat = false;
         })
         .catch((error) => {
           // 処理失敗!not2xx時のコールバック
           console.err(error);
-          this.result_status = 'エラー';
-          this.loading = false;
+          this.result_member.nakcat = { msg: 'エラー' };
+          this.loading_member.nakcat = false;
         });
     },
   },
